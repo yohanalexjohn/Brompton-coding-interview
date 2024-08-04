fc76901ad05c31a602288aa4d6113d83c8169d7d
Yohan John <yohanjohn@Mac-Studio.local>
2024-08-04 16:21:20 +0100

    Finish up basic test functionality for each method
    Update the Cmake to include bits.cpp to build the definitions

--- src/bits.cpp
+++ src/bits.cpp
@@ -1,10 +1,66 @@
 /**
-* @brief 
-* @file
-* 
-* @author Yohan John 
-*/
+ * @brief Defintions and logical setup for the mainpulation
+ *        of bits at the provided location for the given value
+ *
+ * @file bits.cpp
+ * @author Yohan John
+ */
+
 
 #include "bits.hpp"
 
+void bitManipulation::setBit(size_t index)
+{
+    std::lock_guard<std::mutex> lock(mutex);
+    if (index >= size_bitset)
+    {
+        throw std::out_of_range("Index is out of range for the given bitset input");
+    }
 
+    // Vector is stored as 8 bytes
+    size_t byteIndex = index / 8;
+
+    // Find the bit index to jump
+    size_t bitIndex = index % 8;
+
+    // Set the bit at the index for the given value
+    bitset[byteIndex] |= (0x01 << bitIndex);
+}
+
+
+void bitManipulation::clearBit(size_t index)
+{
+    std::lock_guard<std::mutex> lock(mutex);
+    if (index >= size_bitset)
+    {
+        throw std::out_of_range("Index is out of range for the given bitset input");
+    }
+
+    // Vector is stored as 8 bytes
+    size_t byteIndex = index / 8;
+
+    // Find the bit index to jump
+    size_t bitIndex = index % 8;
+
+    // Clear the bit at the index for the given value
+    bitset[byteIndex] &= ~(0x01 << bitIndex);
+
+}
+
+bool bitManipulation::getBit(size_t index)
+{
+    std::lock_guard<std::mutex> lock(mutex);
+    if (index >= size_bitset)
+    {
+        throw std::out_of_range("Index is out of range for the given bitset input");
+    }
+
+    // Vector is stored as 8 bytes
+    size_t byteIndex = index / 8;
+
+    // Find the bit index to jump
+    size_t bitIndex = index % 8;
+
+    // Return the value of the bit at the given index
+    return bitset[byteIndex] & (0x01 << bitIndex);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,21 @@
 /*!
-* @brief 
-* @file
+* @brief Main application and call setup 
 * 
+* @file main.cpp
 * @author Yohan John 
 */
 
-#include <iostream>
-#include <thread>
-#include <vector>
+#include "bits.hpp"
 
 int main()
 {
+    bitManipulation bitset(401);
 
+    bitset.setBit(350);
+
+    std::cout<<"the bit at index position is " << bitset.getBit(350) << std::endl;
+    bitset.clearBit(350); 
+    std::cout<<"the bit at index position is " << bitset.getBit(350) << std::endl;
+
+    return 0;
 }
