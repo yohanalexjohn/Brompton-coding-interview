f26e143b0c6b630b6d4357afc7fb65df997757fa
Yohan John <yohanjohn@Mac-Studio.local>
2024-08-04 12:29:28 +0100

    Create make targets for all tests and docs
    Update the header file with constructor

---
+++ Makefile
@@ -0,0 +1,42 @@
+# Define the Build directory and executable name 
+BUILD_DIR := build
+EXECUTABLE := brompton 
+
+# Determine the operating system
+UNAME_S := $(shell uname -s)
+
+# Define the target all
+all: $(BUILD_DIR)/Makefile
+	$(MAKE) -C $(BUILD_DIR)
+
+# Generate the Makefile using CMake
+$(BUILD_DIR)/Makefile:
+	@mkdir -p $(BUILD_DIR)
+	cmake -S . -B $(BUILD_DIR) 
+
+# Run tests using CTest
+test: all
+	$(MAKE) -C $(BUILD_DIR) test
+
+# Create the updated documentation 
+# Open the html documentaion on the broswer
+docs:
+	doxygen Doxyfile
+	@echo "Documentation generated."
+
+	# Open the HTML documentation based on the OS
+	@if [ "$(UNAME_S)" = "Linux" ]; then \
+		xdg-open docs/html/index.html; \
+	elif [ "$(UNAME_S)" = "Darwin" ]; then \
+		open docs/html/index.html; \
+	elif [ "$(UNAME_S)" = "Windows_NT" ]; then \
+		start docs\\html\\index.html; \
+	else \
+		echo "Unsupported OS: $(UNAME_S)"; \
+	fi
+
+# Clean the build directory
+clean:
+	@rm -rf $(BUILD_DIR)
+
+.PHONY: all test clean docs
--- include/bits.hpp
+++ include/bits.hpp
@@ -8,6 +8,7 @@
 #ifndef BIT_MANIPULATION
 #define BIT_MANIPULATION
 
+#include <vector>
 #include <mutex>
 
 /**
@@ -17,27 +18,56 @@
 class bitManipulation{
     private:
 
+        /** Input Bitset */
+        std::vector<uint8_t> bitset;
+        /** Size of the bitset */
+        size_t size_bitset;
+        /** Mutex Lock to ensure thread safety */
+        std::mutex mutex;
+
+
+        /** 
+         * @brief Calculate the number of bytes required 
+         *         to store the input 
+         * @param input Bitset data  
+         * @return Size required to store the data
+         */
+        size_t input_size(size_t input) const {
+            // +7 to help with the rounding for non multiples of 8 
+            return (input + 7) / 8 ;
+        }
+
     public:
 
         /**
+         * @brief Construct a new bit Manipulation object
+         * 
+         * @param numBits 
+         */
+        bitManipulation(size_t numBits) : size_bitset(numBits)
+        {
+            size_t numBytes = input_size(numBits);
+
+            // Now that we know the size initalise all bits to 0
+            bitset.resize(numBytes,0);
+        }
+
+        /**
         * @brief Sets the bit at the specified index to 1
-        * @param in  -name description
-        *
+        * @param index index position to set the bit at 
         */
         void setBit(size_t index);
 
         /**
         * @brief clears the bit at the specified index
-        * @param in  -name description
-        *
+        * @param index index position to clear the bit at
         */
         void clearBit(size_t index);
 
         /**
         * @brief Returns the value of the bit at the specified index
-        * @param in index -name description
-        * @return -name description
-        *
+        * @param index index position to check the bit value 
+        * @return the bit value at the index
         */
         bool getBit(size_t index);
 
